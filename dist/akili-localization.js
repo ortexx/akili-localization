15575b956dea47297f3f76da9ae6c08638a83917
Alexandr Balasyan <mywebstreet@gmail.com>
2020-09-09 12:12:40 +0300

    upgraded deps

--- dist/akili-localization.js
+++ dist/akili-localization.js
@@ -1,7 +1,7 @@
 /*!
  * Localization service for Akili framework
  * 
- * @version 1.1.3
+ * @version 1.1.4
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link https://github.com/ortexx/akili-localization}
  * {@link https://github.com/ortexx/akili}
