11c2bec19b60fc384dffb3706c75cb328aa39cda
Alexandr Balasyan <mywebstreet@gmail.com>
2020-10-02 14:25:53 +0300

    version 1.2.0

--- dist/akili-localization.js
+++ dist/akili-localization.js
@@ -1,7 +1,7 @@
 /*!
  * Localization service for Akili framework
  * 
- * @version 1.1.4
+ * @version 1.2.0
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link https://github.com/ortexx/akili-localization}
  * {@link https://github.com/ortexx/akili}
